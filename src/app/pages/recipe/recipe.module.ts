408ee610428b98b2f6c8bbbdd72460123c3f2aa2
Bene Martin <h043098@stud.u-szeged.hu>
2022-05-07 02:58:21 +0200

    Third commit

--- src/app/pages/recipe/recipe.module.ts
+++ src/app/pages/recipe/recipe.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { RecipeRoutingModule } from './recipe-routing.module';
 import {RecipeComponent} from "./recipe.component";
 import {MatGridListModule} from "@angular/material/grid-list";
+import {MatListModule} from "@angular/material/list";
 
 
 @NgModule({
   declarations: [
     RecipeComponent
   ],
-  imports: [
-    CommonModule,
-    RecipeRoutingModule,
-    MatGridListModule
-  ]
+    imports: [
+        CommonModule,
+        RecipeRoutingModule,
+        MatGridListModule,
+        MatListModule
+    ]
 })
 export class RecipeModule { }
